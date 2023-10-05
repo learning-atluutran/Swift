005780fed9d2e9e9efbbf68057dc8f94979e4d25
An Luu <atluutran@gmail.com>
2023-10-05 12:33:52 -0400

    Added project LearnSwift-MacOS-Cmdline

---
+++ LearnSwift-MacOS-Cmdline/LearnSwift-MacOS-Cmdline/Language/protocol.swift
@@ -0,0 +1,16 @@
+//
+//  protocol.swift
+//  LearnSwift-MacOS-Cmdline
+//
+//  Created by An Luu on 10/5/23.
+//
+
+import Foundation
+
+protocol MyProtocol {
+    
+}
+
+struct MyStruct: MyProtocol {
+    
+}
---
+++ LearnSwift-MacOS-Cmdline/LearnSwift-MacOS-Cmdline/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  LearnSwift-MacOS-Cmdline
+//
+//  Created by An Luu on 10/5/23.
+//
+
+import Foundation
+
+print("Hello, World!")
+var mp = MyStruct()
+print("Goodbye, World!")
